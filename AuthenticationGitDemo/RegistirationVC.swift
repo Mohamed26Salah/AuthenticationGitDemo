1bb57ccb0975c6dc1a4b8b76bdd4ffcc9c36ca23
Mohamed26Salah <76624455+Mohamed26Salah@users.noreply.github.com>
2023-08-17 11:18:46 +0300

    Registirationscreen Init

---
+++ AuthenticationGitDemo/RegistirationVC.swift
@@ -0,0 +1,21 @@
+//
+//  RegistirationVC.swift
+//  AuthenticationGitDemo
+//
+//  Created by Mohamed Salah on 17/08/2023.
+//
+
+import UIKit
+
+class RegistirationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
